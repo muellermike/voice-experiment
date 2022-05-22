ac6bb712224592d5889166dec6110e9670d53115
muellermike <miki.mueller@hotmail.com>
2022-05-22 14:15:02 +0200

    clear timer when image changes

--- src/components/QuestionImage/QuestionImage.js
+++ src/components/QuestionImage/QuestionImage.js
@@ -24,6 +24,10 @@ class QuestionImage extends React.Component {
                 timeIsUp: false
             });
             
+            if(this.timer) {
+                clearTimeout(this.timer);
+            }
+
             this.timer = setTimeout(() => {
                 this.setState({
                     timeIsUp: true
@@ -33,6 +37,10 @@ class QuestionImage extends React.Component {
     }
 
     componentDidMount() {
+        if(this.timer) {
+            clearTimeout(this.timer);
+        }
+        
         this.timer = setTimeout(() => {
             this.setState({
                 timeIsUp: true
