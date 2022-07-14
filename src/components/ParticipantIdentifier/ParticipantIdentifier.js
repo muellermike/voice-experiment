14f9422d04c2669544ee0191b42d485a6e5c238f
muellermike <miki.mueller@hotmail.com>
2022-07-14 22:40:45 +0200

    simplify participant identifier as no form

--- src/components/ParticipantIdentifier/ParticipantIdentifier.js
+++ src/components/ParticipantIdentifier/ParticipantIdentifier.js
@@ -22,21 +22,12 @@ function ParticipantIdentifier() {
         }
     }, [query])
 
-    const handleSumbit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        } else {
-            dispatch(storeExternalUserId(extUserId));
-            if (query.get("img_tm")) {
-                console.log("img_tm: " + query.get("img_tm"));
-                dispatch(storeImageTime(query.get("img_tm") * 1000));
-            }
-            navigate("/participant");
+    const handleSumbit = () => {
+        dispatch(storeExternalUserId(extUserId));
+        if (query.get("img_tm")) {
+            dispatch(storeImageTime(query.get("img_tm") * 1000));
         }
-
-        setValidated(true);
+        navigate("/participant");
     }
 
     /*
@@ -47,11 +38,9 @@ function ParticipantIdentifier() {
             <Card>
                 <Card.Title>Participate in the experiment</Card.Title>
                 <Card.Body>
-                <p>This is the user interface in which the described experiment takes place. Please take your time to perform the experiment.</p>
+                    <p>This is the user interface in which the described experiment takes place. Please take your time to perform the experiment.</p>
                     <p>To participate in the experiment, please choose next.</p>
-                    <Form noValidate validated={validated} onSubmit={handleSumbit}>
-                        <Button variant="primary" style={{ margin: "25px"}} type="submit">Next</Button>
-                    </Form>
+                    <Button variant="primary" style={{ margin: "25px"}} onClick={handleSumbit} type="submit">Next</Button>
                 </Card.Body>
             </Card>
         </div>
